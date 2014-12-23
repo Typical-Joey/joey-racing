46f4588e744e1b54a728a4d685aa0050539a00c2
Joseph Capocci <jtcapocci@yahoo.com>
2014-12-22 23:13:37 -0600

    add accel and made falling objects

--- RacingGame.py
+++ RacingGame.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-
+import random
 pygame.init()
 
 display_width = 800
@@ -17,6 +17,8 @@ clock = pygame.time.Clock()
 carImg = pygame.image.load('racecar.png')
 carImgSize = carImg.get_rect().size
 
+accel = [0, 0]
+
 def crash():
     message_display('You Crashed')
 
@@ -30,17 +32,26 @@ def message_display(text):
     TextRect.center = ((display_width / 2), (display_height / 2))
     gameDisplay.blit(TextSurf, TextRect)
     pygame.display.update()
-    time.sleep(2)
+    time.sleep(1)
     game_loop()
 
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
 
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
+
 def game_loop():
     x = (display_width * 0.45)
     y = (display_height * 0.6)
 
-    pos_change = [0, 0]
+    speed = [0, 0]
+
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 200
+    thing_height = 100
 
     gameExit = False
 
@@ -53,30 +64,39 @@ def game_loop():
 
             if etype == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    pos_change[0] = -5
+                    accel[0] -= 1
                 elif event.key == pygame.K_RIGHT:
-                    pos_change[0] = 5
-                elif event.key == pygame.K_UP:
-                    pos_change[1] = -5
+                    accel[0] += 1
+                if event.key == pygame.K_UP:
+                    accel[1] -= 1
                 elif event.key == pygame.K_DOWN:
-                    pos_change[1] = 5
+                    accel[1] += 1
 
             if etype == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    pos_change[0] = 0
+                    speed[0] = accel[0] = 0
                 if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    pos_change[1] = 0
+                    speed[1] = accel[1] = 0
 
-        x += pos_change[0]
-        y += pos_change[1]
+        speed[0] += accel[0]
+        speed[1] += accel[1]
+        x += speed[0]
+        y += speed[1]
 
         gameDisplay.fill(white)
+
+
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
         car(x, y)
 
         if x > (display_width - carImgSize[0]) or x < 0:
             crash()
         if y > (display_height - carImgSize[1]) or y < 0:
             crash()
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0,display_width)
 
         pygame.display.update()
         clock.tick(60)
